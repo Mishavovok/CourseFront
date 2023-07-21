2796fb4ff66d0464b6fe2947659d3d7ed6a8fc70
Mikhail Vovk <83818466+Mishavovok@users.noreply.github.com>
2023-07-21 12:42:55 +0300

    finish

---
+++ mail.php
@@ -0,0 +1,55 @@
+<?php
+// Подключение библиотеки
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Получение данных
+$json = file_get_contents('php://input'); // Получение json строки
+$data = json_decode($json, true); // Преобразование json
+
+// Данные
+$name = $data['name'];
+$phone = $data['phone'];
+$email = $data['email'];
+
+// Контент письма
+$title = 'Заявка с сайта'; // Название письма
+$body = '<p>Имя и Фамилия: <strong>'.$name.'</strong></p>'.
+        '<p>Телефон: <strong>'.$phone.'</strong></p>'.
+        '<p>Email: <strong>'.$email.'</strong></p>';
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+try {
+  $mail->isSMTP();
+  $mail->CharSet = 'UTF-8';
+  $mail->SMTPAuth   = true;
+
+  // Настройки почты отправителя
+  $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+  $mail->Username   = 'misavovk878@gmail.com'; // Логин на почте
+  $mail->Password   = 'rowiblsgydgdsqdv'; // Пароль на почте
+  $mail->SMTPSecure = 'ssl';
+  $mail->Port       = 465;
+
+  $mail->setFrom('misavovk878@gmail.com', 'Заявка с сайта'); // Адрес самой почты и имя отправителя
+
+  // Получатель письма
+  $mail->addAddress('misavovk878@gmail.com');
+
+  // Отправка сообщения
+  $mail->isHTML(true);
+  $mail->Subject = $title;
+  $mail->Body = $body;
+
+  $mail->send('d');
+
+  // Сообщение об успешной отправке
+  echo ('Сообщение отправлено успешно!');
+
+} catch (Exception $e) {
+  header('HTTP/1.1 400 Bad Request');
+  echo('Сообщение не было отправлено! Причина ошибки: {$mail->ErrorInfo}');
+}
---
+++ main.js
@@ -0,0 +1,66 @@
+const errorMsg = document.querySelector('#error-msg');
+const validMsg = document.querySelector('#valid-msg');
+const errorMap = [
+  'Неправильный номер',
+  'Неверный код страны',
+  'Слишком короткий',
+  'Слишком долинный',
+  'Неправильный номер',
+];
+const iti = window.intlTelInput(input, {
+  utilsScript: '/intl-tel-input/js/utils.js?1687509211722',
+});
+const reset = () => {
+  input.classList.remove('error');
+  errorMsg.innerHTML = '';
+  errorMsg.classList.add('hide');
+  validMsg.classList.add('hide');
+};
+
+input.addEventListener('blur', () => {
+  reset();
+  if (input.value.trim()) {
+    if (iti.isValidNumber()) {
+      validMsg.classList.remove('hide');
+    } else {
+      input.classList.add('error');
+      const errorCode = iti.getValidationError();
+      errorMsg.innerHTML = errorMap[errorCode];
+      errorMsg.classList.remove('hide');
+    }
+  }
+});
+const emailInput = document.querySelector('#email');
+const emailInputresult = document.querySelector('#result');
+emailInput.addEventListener('input', (e) => {
+  const emailInputValue = e.currentTarget.value;
+  if (/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(emailInputValue) != true) {
+    emailInputresult.textContent = 'Неправильный E-mail';
+  } else {
+    emailInputresult.textContent = '';
+  }
+});
+const nameInput = document.querySelector('#name');
+const formElement = document.getElementById('form');
+formElement.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let data = {
+    name: nameInput.value,
+    phone: input.value,
+    email: emailInput.value,
+  };
+  let response = fetch('mail.php', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+  });
+  let result = response.text();
+
+  alert(result);
+  // const phone = input.value;
+  // const name =  nameInput.value;
+  // const email = emailInput.value;
+  // console.log(name, phone, email);
+});
