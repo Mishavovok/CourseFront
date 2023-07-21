55e96e7602401b1d815dc82885f4f38069107a3d
Mikhail Vovk <83818466+Mishavovok@users.noreply.github.com>
2023-07-21 16:07:34 +0300

    up get

--- main.js
+++ main.js
@@ -49,7 +49,7 @@ form.addEventListener('submit', function (event) {
   console.log(formData);
 
   fetch('https://course-front-six.vercel.app/mail.php', {
-    method: 'POST',
+    method: 'get',
     body: formData,
   })
     .then((response) => {
